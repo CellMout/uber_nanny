e98d405b159100e40da81741d0d4c4d66b344826
Elie Celka <elie.celka@gmail.com>
2024-12-02 17:41:35 +0100

    basic seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+Booking.destroy_all
+Nanny.destroy_all
 User.destroy_all
 
 User.create(email: "elie.celka@gmail.com", password: "password")
@@ -18,6 +20,19 @@ User.create(email: "elie.celka@gmail.com", password: "password")
   User.create(email: user["results"][0]["email"], password: "password")
 end
 
-User.all
+
+User.all.each do |user|
+  2.times do
+    url = "https://randomuser.me/api/"
+    user_serialized = URI.parse(url).read
+    data = JSON.parse(user_serialized)
+    user.nannies << Nanny.create(name: data["results"][0]["name"]["first"], description: Faker::Lorem.paragraph, hour_rate: rand(8.0..25.5).round(1), photo_url: data["results"][0]["picture"]["large"])
+  end
+end
+User.all.each do |user|
+  Booking.create(user: user, address: Faker::Address.street_address + ", city: " + Faker::Address.city, nanny: Nanny.all.sample, start_time: DateTime.now, end_time: DateTime.parse("2024-12-30T21:20:44+09:00"), status: "pending")
+end
 
 puts "created #{User.count} users"
+puts "created #{Nanny.count} nannies"
+puts "created #{Booking.count} bookings"
