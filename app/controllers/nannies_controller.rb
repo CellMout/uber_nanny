4a7c2fbe7fc78a690fd741db7cfa1563c7d34ddd
benjamin masson <benjamin.masson@gmail.com>
2024-12-03 17:21:17 +0100

    cloudinary quasi bon

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -12,10 +12,24 @@ class NanniesController < ApplicationController
     @nanny = Nanny.new
   end
 
+    if @nanny.save
+      redirect_to @nanny, notice: 'Nanny successfully created!'
+    else
+      render :new
+    end
+  end
+
   def create
     @nanny = Nanny.new(nanny_params)
-    @nanny.save
-    redirect_to nanny_path(@nanny)
+    if params[:nanny][:photo].present?
+      uploaded_file = params[:nanny][:photo]
+      upload_result = Cloudinary::Uploader.upload(uploaded_file.path)
+      @nanny.photo_url = upload_result['secure_url']
+      @nanny.save
+      redirect_to @nanny, notice: 'Nanny successfully created!'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -29,7 +43,8 @@ class NanniesController < ApplicationController
   end
 
   private
+
   def nanny_params
-    params.require(:nanny).permit(:name, :description)
+    params.require(:nanny).permit(:name, :description, :photo_url)
   end
 end
