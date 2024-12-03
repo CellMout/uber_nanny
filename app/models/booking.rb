f0798a2bb3ca7de02c18f2a5191bce196375ebd8
Elie Celka <elie.celka@gmail.com>
2024-12-03 11:20:21 +0100

    booking status validates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,11 @@
 class Booking < ApplicationRecord
   belongs_to :nanny
   belongs_to :user
+
+  STATUS = ["pending", "accepted", "declined"]
+
+  validates :status, inclusion: { in: STATUS }
+
+  def duration
+  end
 end
