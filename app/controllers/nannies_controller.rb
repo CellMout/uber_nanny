3a53dd8fd8bc029c466536f96432aaf200d363aa
benjamin masson <benjamin.masson@gmail.com>
2024-12-04 11:53:54 +0100

    cloudinary_done

--- app/controllers/nannies_controller.rb
+++ app/controllers/nannies_controller.rb
@@ -18,14 +18,11 @@ class NanniesController < ApplicationController
   def create
     @nanny = Nanny.new(nanny_params)
 
-    if params[:nanny][:photo].present?
-      uploaded_file = params[:nanny][:photo]
-      upload_result = Cloudinary::Uploader.upload(uploaded_file.path)
-      @nanny.photo_url = upload_result['secure_url']
-      @nanny.save
+    if @nanny.save!
       redirect_to @nanny, notice: 'Nanny successfully created!'
     else
-      render :new, alert: "Creation error"
+      render :new, status: :unprocessable_entity
+      flash[:alert] = "Creation error"
     end
   end
 
@@ -54,6 +51,6 @@ class NanniesController < ApplicationController
   end
 
   def nanny_params
-    params.require(:nanny).permit(:name, :description, :hour_rate, :photo_url)
+    params.require(:nanny).permit(:name, :description, :hour_rate, :photo)
   end
 end
